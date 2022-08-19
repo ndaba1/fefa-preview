2eeaee4fe6a7f291d97ac74f1602b1e281de417a
Victor Ndaba <mwangindaba@gmail.com>
2022-08-19 19:23:39 +0300

    refactor: changed hero component

--- components/sections/HomePageHero.tsx
+++ components/sections/HomePageHero.tsx
@@ -1,6 +1,5 @@
 import { Button, Text } from '@mantine/core';
 import { NextLink } from '@mantine/next';
-import GradientText from 'components/GradientText';
 import HeroContainer from 'components/HeroContainer';
 import { useFefaAssets } from 'hooks/styles';
 import Image from 'next/image';
@@ -10,16 +9,10 @@ export default function HomePageHero() {
     <HeroContainer
       left={
         <div className="w-full space-y-5">
-          <Text
-            className="font-roboto text-4xl leading-tight md:text-[3.3rem] font-bold md:leading-[70px]"
-            component="h1"
-          >
-            Providing access to
-            <GradientText text="free" from="teal" to="cyan" /> and{' '}
-            <GradientText text="high quality" from="teal" to="indigo" />
-            education for all.
-          </Text>
-          <Text color={'dimmed'} component="p" className="text-xl md:text-2xl">
+          <h2 className="font-roboto text-4xl leading-tight md:text-[3.3rem] font-bold md:leading-[70px]">
+            Providing access to free and high quality education for all.
+          </h2>
+          <Text color="dimmed" component="p" className="text-xl md:text-2xl">
             An online academy with a focus on availing knowledge to everyone, for free. Free in
             every sense of the word.
           </Text>
@@ -47,6 +40,6 @@ export default function HomePageHero() {
           />
         </div>
       }
-    ></HeroContainer>
+    />
   );
 }
